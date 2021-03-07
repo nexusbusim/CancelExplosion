8ff7eed0c0517c3965ae7faa1ca5adeefe93d0ef
nexusbusim <55572245+nexusbusim@users.noreply.github.com>
2021-03-07 23:05:58 +0900

    Add files via upload

--- CancelExp/src/com/khwoo/Commands.java
+++ CancelExp/src/com/khwoo/Commands.java
@@ -7,12 +7,12 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 
 public class Commands implements CommandExecutor{
 	
 	FileConfiguration config = Main.getPlugin(Main.class).getConfig(); // get config from Main class
 	Plugin plugin = Bukkit.getPluginManager().getPlugin("CancelExplosion");
+
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -21,15 +21,18 @@ public class Commands implements CommandExecutor{
 		Permission acSwitch = new Permission("ac.switch");
 		Permission acReload = new Permission("ac.reload");
 		
+		String language = config.getString("lang"); // get language information
+		
 		if(cmd.getName().equalsIgnoreCase("ac")){
 			if(args.length == 1) { // check command argument length is 1
 				if(args[0].equalsIgnoreCase("info") && sender.hasPermission(acInfo)) {  // /ac info
 					sender.sendMessage("============\n");
-					sender.sendMessage("버전 : 0.0.4 폭발방지\n");
+					sender.sendMessage(config.getString(language.concat(".version"))); // show version information
+					sender.sendMessage("\n");
 					if(config.getBoolean("state")) {
-						sender.sendMessage("상태 :" + ChatColor.BLUE + " 켜짐\n"); // Enabled == Blue
+						sender.sendMessage(config.getString(language.concat(".statusOn"))); // Enabled == Blue
 					} else {
-						sender.sendMessage("상태 :" + ChatColor.RED + " 꺼짐\n"); // Disabled == Red
+						sender.sendMessage(config.getString(language.concat(".statusOff"))); // Disabled == Red
 					}
 					sender.sendMessage("============\n");
 					
@@ -37,28 +40,28 @@ public class Commands implements CommandExecutor{
 				} else if (args[0].equalsIgnoreCase("on") && sender.hasPermission(acSwitch)) { //  /ac on
 						config.set("state", true);
 						Main.getPlugin(Main.class).saveConfig();
-						sender.sendMessage("폭발방지"+ ChatColor.BLUE+ " 켜짐\n");
+						sender.sendMessage(config.getString(language.concat(".statusOn"))); // switch ON
 						
 						return true;
 				} else if (args[0].equalsIgnoreCase("off") && sender.hasPermission(acSwitch)) { // /ac off
 						config.set("state", false);
 						Main.getPlugin(Main.class).saveConfig();
-						sender.sendMessage("폭발방지"+ ChatColor.RED + " 꺼짐\n");
+						sender.sendMessage(config.getString(language.concat(".statusOff"))); // switch OFF
 					
 					return true;
 				}else if (args[0].equalsIgnoreCase("reload")  && sender.hasPermission(acReload)) { // /ac reload
 					Bukkit.getPluginManager().disablePlugin(plugin);
 					Bukkit.getPluginManager().enablePlugin(plugin);
 										
-					sender.sendMessage("폭발방지 설정파일 리로딩\n");
+					sender.sendMessage(config.getString(language.concat(".reload")));
 
 					
 					return true;
 				} else {
-					sender.sendMessage("잘못된 커맨드를 입력했거나, 권한이 없습니다.");
+					sender.sendMessage("YOU DONT HAVE PERMISSION FOR THIS");
 				}
 			} else {
-				sender.sendMessage("잘못된 커맨드를 입력했습니다.");
+				sender.sendMessage("WRONG COMMAND");
 			}
 		}
 		
--- CancelExp/src/com/khwoo/Main.java
+++ CancelExp/src/com/khwoo/Main.java
@@ -8,13 +8,37 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin{
 	
 	FileConfiguration config = getConfig(); // initiate config object
+
 	
 	@Override 
 	public void onEnable() {
-		Bukkit.getConsoleSender().sendMessage("폭발 방지 플러그인 실행, 버전 : 0.0.4");
 		this.getCommand("ac").setExecutor(new Commands()); // get command from Commands() class
 		
+		config.addDefault("lang", "ko"); // ko : korean , en : english
+		String language = config.getString("lang"); // get language information
+		
+		Bukkit.getConsoleSender().sendMessage(config.getString(language.concat(".onEnable"))); // onEnable
+
 		config.addDefault("state", true); // default config to true
+
+		// language support for english
+		config.addDefault("en.onEnable", "CancelExplosion Enabled, version : 0.0.4"); // .onEnable
+		config.addDefault("en.onDisable", "CancelExplosion Enabled, version : 0.0.4"); // .onDisable
+		config.addDefault("en.version", "Current version : 0.0.4 CancelExplosion"); // version information
+		config.addDefault("en.statusOn", "Current status : ON"); // on
+		config.addDefault("en.statusOff", "Current status : OFF"); // off
+		config.addDefault("en.reload", "reloading config file"); // reload
+		
+		
+		// language support for korean
+		config.addDefault("ko.onEnable", "폭발 방지 플러그인 실행, 버전 : 0.0.4"); // .onEnable
+		config.addDefault("ko.onDisable", "폭발 방지 플러그인 종료"); // .onDisable
+		config.addDefault("ko.version", "버전 : 0.0.4 폭발방지"); // version information
+		config.addDefault("ko.statusOn", "현재 상태 : 켜짐"); // on
+		config.addDefault("ko.statusOff", "현재 상태 : 꺼짐"); // off
+		config.addDefault("ko.reload", "설정파일 리로딩"); // reload
+		
+		
 		config.options().copyDefaults(true); // save config
 		saveConfig();
 		
@@ -23,7 +47,8 @@ public class Main extends JavaPlugin{
 	
 	@Override
 	public void onDisable() {
-		Bukkit.getConsoleSender().sendMessage("폭발 방지 플러그인 종료");
+		String language = config.getString("lang"); // get language information
+		Bukkit.getConsoleSender().sendMessage(config.getString(language.concat(".onDisable"))); // onDisable
 		reloadConfig(); // reload config file 
 	}
 
