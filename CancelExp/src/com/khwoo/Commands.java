e729a3be90e502bdcd527f7c81bfaf038ef13364
nexusbusim <55572245+nexusbusim@users.noreply.github.com>
2021-03-10 21:01:09 +0900

    Add files via upload

--- CancelExp/src/com/khwoo/Commands.java
+++ CancelExp/src/com/khwoo/Commands.java
@@ -17,48 +17,56 @@ public class Commands implements CommandExecutor{
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		
-		Permission acInfo = new Permission("ac.info");
-		Permission acSwitch = new Permission("ac.switch");
-		Permission acReload = new Permission("ac.reload");
-		
 		String language = config.getString("lang"); // get language information
 		
 		if(cmd.getName().equalsIgnoreCase("ac")){
-			if(args.length == 1) { // check command argument length is 1
-				if(args[0].equalsIgnoreCase("info") && sender.hasPermission(acInfo)) {  // /ac info
-					sender.sendMessage("============\n");
-					sender.sendMessage(config.getString(language.concat(".version"))); // show version information
-					sender.sendMessage("\n");
-					if(config.getBoolean("state")) {
-						sender.sendMessage(config.getString(language.concat(".statusOn"))); // Enabled == Blue
-					} else {
-						sender.sendMessage(config.getString(language.concat(".statusOff"))); // Disabled == Red
-					}
-					sender.sendMessage("============\n");
-					
-					return true;
-				} else if (args[0].equalsIgnoreCase("on") && sender.hasPermission(acSwitch)) { //  /ac on
-						config.set("state", true);
-						Main.getPlugin(Main.class).saveConfig();
-						sender.sendMessage(config.getString(language.concat(".statusOn"))); // switch ON
+			if(args.length == 1) { 
+				switch (args[0]) {
+					case "info": // ac info | show information 
+						if(sender.hasPermission(new Permission("ac.info"))) {
+							sender.sendMessage("============\n");
+							sender.sendMessage(config.getString(language.concat(".version"))); // show version information
+							sender.sendMessage("\n");
+							if(config.getBoolean("state")) {
+								sender.sendMessage(config.getString(language.concat(".statusOn"))); // Enabled == Blue
+							} else {
+								sender.sendMessage(config.getString(language.concat(".statusOff"))); // Disabled == Red
+							}
+							sender.sendMessage("============\n");
+							return true;
+						}
+						break;
 						
-						return true;
-				} else if (args[0].equalsIgnoreCase("off") && sender.hasPermission(acSwitch)) { // /ac off
-						config.set("state", false);
-						Main.getPlugin(Main.class).saveConfig();
-						sender.sendMessage(config.getString(language.concat(".statusOff"))); // switch OFF
-					
-					return true;
-				}else if (args[0].equalsIgnoreCase("reload")  && sender.hasPermission(acReload)) { // /ac reload
-					Bukkit.getPluginManager().disablePlugin(plugin);
-					Bukkit.getPluginManager().enablePlugin(plugin);
-										
-					sender.sendMessage(config.getString(language.concat(".reload")));
-
-					
-					return true;
-				} else {
-					sender.sendMessage("YOU DONT HAVE PERMISSION FOR THIS");
+					case "on": // ac on | enable plugin
+						if(sender.hasPermission(new Permission("ac.switch"))) {
+							config.set("state", true);
+							Main.getPlugin(Main.class).saveConfig();
+							sender.sendMessage(config.getString(language.concat(".statusOn"))); // switch ON
+							return true;
+						}
+						break;
+						
+					case "off": // ac off | disable plugin
+						if(sender.hasPermission(new Permission("ac.switch"))) {
+							config.set("state", false);
+							Main.getPlugin(Main.class).saveConfig();
+							sender.sendMessage(config.getString(language.concat(".statusOff"))); // switch OFF
+							return true;
+						}
+						break;
+						
+					case "reload": // ac reload | reload config file
+						if(sender.hasPermission(new Permission("ac.reload"))) {
+							Bukkit.getPluginManager().disablePlugin(plugin);
+							Bukkit.getPluginManager().enablePlugin(plugin);	
+							sender.sendMessage(config.getString(language.concat(".reload")));
+							return true;
+						}
+						break;
+						
+					default:
+						sender.sendMessage("YOU DONT HAVE PERMISSION FOR THIS");
+						break;
 				}
 			} else {
 				sender.sendMessage("WRONG COMMAND");
--- CancelExp/src/com/khwoo/Main.java
+++ CancelExp/src/com/khwoo/Main.java
@@ -15,25 +15,26 @@ public class Main extends JavaPlugin{
 		this.getCommand("ac").setExecutor(new Commands()); // get command from Commands() class
 		
 		config.addDefault("lang", "ko"); // ko : korean , en : english
-		String language = config.getString("lang"); // get language information
+		
+		String language = config.getString("lang"); // get language information AFTER SET LANGUAGE CONFIG
 		
 		Bukkit.getConsoleSender().sendMessage(config.getString(language.concat(".onEnable"))); // onEnable
 
 		config.addDefault("state", true); // default config to true
 
 		// language support for english
-		config.addDefault("en.onEnable", "CancelExplosion Enabled, version : 0.0.4"); // .onEnable
-		config.addDefault("en.onDisable", "CancelExplosion Enabled, version : 0.0.4"); // .onDisable
-		config.addDefault("en.version", "Current version : 0.0.4 CancelExplosion"); // version information
+		config.addDefault("en.onEnable", "CancelExplosion Enabled, version : 0.0.5"); // .onEnable
+		config.addDefault("en.onDisable", "CancelExplosion Disabled"); // .onDisable
+		config.addDefault("en.version", "Current version : 0.0.5 CancelExplosion"); // version information
 		config.addDefault("en.statusOn", "Current status : ON"); // on
 		config.addDefault("en.statusOff", "Current status : OFF"); // off
 		config.addDefault("en.reload", "reloading config file"); // reload
 		
 		
 		// language support for korean
-		config.addDefault("ko.onEnable", "폭발 방지 플러그인 실행, 버전 : 0.0.4"); // .onEnable
+		config.addDefault("ko.onEnable", "폭발 방지 플러그인 실행, 버전 : 0.0.5"); // .onEnable
 		config.addDefault("ko.onDisable", "폭발 방지 플러그인 종료"); // .onDisable
-		config.addDefault("ko.version", "버전 : 0.0.4 폭발방지"); // version information
+		config.addDefault("ko.version", "버전 : 0.0.5 폭발방지"); // version information
 		config.addDefault("ko.statusOn", "현재 상태 : 켜짐"); // on
 		config.addDefault("ko.statusOff", "현재 상태 : 꺼짐"); // off
 		config.addDefault("ko.reload", "설정파일 리로딩"); // reload
